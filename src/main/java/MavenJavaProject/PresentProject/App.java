5d27f5a5e0149ecb4efb10bcc835a725014b016e
Gupta-01 <saiprathyun85@gmail.com>
2024-11-25 15:59:13 +0530

    Add files via upload

---
+++ src/main/java/MavenJavaProject/PresentProject/App.java
@@ -0,0 +1,13 @@
+package MavenJavaProject.PresentProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello world");
+    }
+}
